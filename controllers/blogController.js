ad4b03407b1b268d92e8188885d50e652ef84ef3
exismys <riteshzeronity@gmail.com>
2021-09-17 23:33:11 +0530

    Set up the blogDetail route and view + set the UI to show only the first three lines of content

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,8 +1,8 @@
-var Book = require("../models/blog");
+var Blog = require("../models/blog");
 
 // Blog home page: shows all the blogs saved
 exports.blogList = function(req, res) {
-    Book.find({}, "title author date content").exec(
+    Blog.find({}, "title author date content").exec(
         function(error, blog_lists) {
             if (error) {
                 console.log("Error in retriving blogs!");
@@ -14,7 +14,17 @@ exports.blogList = function(req, res) {
 }
 
 // Shows the details of a specific blog
-exports.blogDetail = function(req, res) {}
+exports.blogDetail = function(req, res) {
+    Blog.findById(req.params.id).exec(
+        function(error, blog) {
+            if (error) {
+                console.log(`Error in retrieving book: ${req.params.id}`);
+            } else {
+                res.render("blogDetail", {blog: blog});
+            }
+        }
+    )
+}
 
 // Create a new blog
 exports.createBlogGet = function(req, res) {}
--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,10 @@ router.get('/', function(req, res, next) {
 
 // Project routes
 router.get('/projects/', projectController.projectList);
+router.get('/project/:id', projectController.projectDetail);
 
 // Blog routes
 router.get('/blog/', blogController.blogList);
+router.get('/blog/:id', blogController.blogDetail);
 
 module.exports = router;
