88e5e42439a217939bf571a804fce0daa19e0e1b
exismys <riteshzeronity@gmail.com>
2021-12-17 12:25:00 +0530

    added subroutine to check authentication

--- public/javascripts/checkAuth.js
+++ public/javascripts/checkAuth.js
@@ -1,16 +1,28 @@
-document.getElementById('authbtn').addEventListener('click', (event) => {
-    const result = fetch('/checkAuth/', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            token: localStorage.getItem('token')
-        })
-    }).then(res => res.json());
-    if (result.status = 'ok') {
-        console.log('authorized');
-    } else {
-        alert(result.error);
-    }
+document.getElementById("authbtn").addEventListener("click", (event) => {
+  if (isAutherized) {
+    alert("User is authorized.");
+  } else {
+    alert("Not autherized!");
+  }
 });
+
+let isAutherized = function (event) {
+  const result = fetch("/checkAuth/", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("token"),
+    }),
+  }).then((res) => res.json());
+
+  if ((result.status = "ok")) {
+    return true;
+  } else {
+    console.log(result.error);
+    return false;
+  }
+};
+
+export default isAutherized;
